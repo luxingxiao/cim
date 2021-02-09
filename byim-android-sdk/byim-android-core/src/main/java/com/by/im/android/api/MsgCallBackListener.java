dfda1bf30ede22b28dbccd15f35be24675a746eb
luxingxiao <luxingxiao@idstaff.com>
2021-02-09 10:15:33 +0800

    将android sdk作为独立项目维护

--- byim-android-sdk/byim-android-core/src/main/java/com/by/im/android/api/MsgCallBackListener.java
+++
@@ -1,8 +0,0 @@
-package com.by.im.android.api;
-
-/**
- * @author luxingxiao
- */
-public interface MsgCallBackListener {
-    void handle(long userId, String message);
-}
--- byim-android-sdk/byim-common/src/main/java/com/by/im/common/res/NULLBody.java
+++
@@ -1,9 +0,0 @@
-package com.by.im.common.res;
-
-public class NULLBody {
-    public NULLBody() {}
-
-    public static NULLBody create(){
-        return new NULLBody();
-    }
-}
